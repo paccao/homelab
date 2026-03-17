83b78c43c56a4f8189a7a74167ccd231a1ff0479
paccao <16489683+paccao@users.noreply.github.com>
2026-03-17 22:21:21 +0100

    Cilium WiP
    Got cilium up and running, last issues to fix in cilium-operator for gateway API and l2 ip pools loadbalancing support

---
+++ crds/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required CRDs for Cilium GatewayApi https://docs.cilium.io/en/v1.18/network/servicemesh/gateway-api/gateway-api/
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_gateways.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_grpcroutes.yaml
+
+# TLSRoute
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
---
+++ infra/cilium/README.md
@@ -0,0 +1,38 @@
+# Installation instructions
+
+Install with CRDs:
+
+```bash
+helm template \
+  cilium \
+  cilium/cilium \
+  --version 1.18.4 \
+  -f values.yaml \
+  --include-crds > secrets/cilium.yaml
+```
+
+If CRDs are already installed, skip the --include-crds flag above.
+
+```bash
+helm template \
+  cilium \
+  cilium/cilium \
+  --version 1.18.4 \
+  -f values.yaml > secrets/cilium.yaml
+```
+
+# IMPORTANT
+
+Remove the `kind: Secret` objects inside of cilium.yaml.
+
+Seal them into `cilium-ca-sealed.yaml` and `hubble-server-certs-sealed.yaml`
+
+```bash
+cat secrets/cilium-ca.yaml | kubeseal --controller-namespace sealed-secrets --format yaml -w cilium-ca-sealed.yaml
+
+cat secrets/hubble-server-certs.yaml | kubeseal --controller-namespace sealed-secrets --format yaml -w hubble-server-certs-sealed.yaml
+```
+
+Put cilium.yaml outside of the secrets/ directory when it is free of secrets.
+
+Then, apply: `k apply -k cilium/`
